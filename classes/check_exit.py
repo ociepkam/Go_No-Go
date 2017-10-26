6819f8e47119f4edf7baaab48a1823b021c2f9bd
poia <ociepka.m@gmail.com>
2017-10-26 17:11:01 +0200

    saving data in check exit

--- classes/check_exit.py
+++ classes/check_exit.py
@@ -1,8 +1,12 @@
 from psychopy import event, logging
+from classes.save_data import save_beh, save_triggers
 
 
-def check_exit(key='f7'):
+def check_exit(key='f7', part_id=None, beh=None, triggers_list=None):
     stop = event.getKeys(keyList=[key])
     if len(stop) > 0:
+        if part_id:
+            save_beh(beh, part_id)
+            save_triggers(triggers_list, part_id)
         logging.critical('Experiment finished by user! {} pressed.'.format(key))
         exit(1)
